8ea490c3169bb3284ed177b9522f9845bb523250
Andrey Gavrilyuk <andrew250519@gmail.com>
2023-01-09 21:23:24 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,19 @@ export default class App extends Component {
     number: '',
   };
 
-  handleNameChange = e => {
-    this.setState({ name: e.currentTarget.value });
+  handleInputChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
   };
+  // handleNameChange = e => {
+  //   this.setState({ name: e.currentTarget.value });
+  // };
 
-  handleNumberChange = e => {
-    this.setState({ number: e.currentTarget.value });
-  };
+  // handleNumberChange = e => {
+  //   this.setState({ number: e.currentTarget.value });
+  // };
 
   render() {
     return (
@@ -29,19 +35,19 @@ export default class App extends Component {
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
               value={this.state.name}
-              onChange={this.handleNameChange}
+              onChange={this.handleInputChange}
             />
           </label>
           <label>
             Number
             <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
               value={this.state.number}
-              onChange={this.handleNumberChange}
+              onChange={this.handleInputChange}
             />
           </label>
         </form>
